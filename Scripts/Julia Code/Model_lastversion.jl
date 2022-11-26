d443d1e7e2e5e187f8980cd00aada533c83af1ab
renatoquiliche <renato.quiliche@pucp.pe>
2022-11-26 18:34:03 -0300

    Date

--- Scripts/Julia Code/Model_lastversion.jl
+++ Scripts/Julia Code/Model_lastversion.jl
@@ -1,12 +1,13 @@
-using JuMP,  CSV, DataFrames, Distributions, StatsBase, Random, Plots, COPT, QuasiMonteCarlo, HiGHS, StatsPlots, HypothesisTests
+using JuMP,  CSV, DataFrames, Distributions, StatsBase, Random, Plots, QuasiMonteCarlo, HiGHS, StatsPlots, HypothesisTests
 
 
 
-function estima_contrato(D::Matrix)
+function estima_contrato(D::Matrix, α, λ, τ, Ti, Tf)
     S,N = size(D)
     p = ones(S) * 1/S
 
-    model = Model(HiGHS.Optimizer)
+    model = Model()
+    model = Model(optimizer_with_attributes(HiGHS.Optimizer,  "parallel" => "on"))
     set_silent(model)
 
     @variable(model, T[1:3] ≥ 0) #valores limites do contrato e restricao A.25
@@ -31,7 +32,8 @@ function estima_contrato(D::Matrix)
     @objective(model, Max, π - (((1 - λ)*sum(p[s] * I[s] for s in 1:S)) + λ*(v + sum(p[s]*β[s]/(1 - α) for s in 1:S))))
 
     #@constraint(model, π ≤ (1 + τ)*sum(p[s]*I[s] for s in 1:S)) #A.2 (premio maior do que a idenizacao media)
-    @constraint(model, π ≤ 2.5*sum(p[s]*I[s] for s in 1:S)) #restringindo o premio em funcao da indenizacao media
+    @constraint(model, π ≤ τ*sum(p[s]*I[s] for s in 1:S)) #restringindo o premio em funcao da indenizacao media
+    #@constraint(model, π ≤ 2.5*sum(p[s]*I[s] for s in 1:S)) #restringindo o premio em funcao da indenizacao media
     @constraint(model, π ≥ 0.7*sum(p[s]*I[s] for s in 1:S)) #restringindo o premio em funcao da indenizacao media
     @constraint(model, T[2] ≤ T[3]) #A.3
     @constraint(model, Ti ≤ T[1] ≤ Tf) #A.4
@@ -86,7 +88,7 @@ function MC_bootstrap(y, sample_size)
 end
 
 function QMC_LHS_empirical(data, sample_size, N, p, type)
-    s = zeros(sample_size, N)
+    s_aux = zeros(sample_size, N)
     cluster = Int64.(zeros(sample_size))
     ran = zeros(sample_size,N)
     idx = zeros(sample_size, N)
@@ -108,16 +110,28 @@ function QMC_LHS_empirical(data, sample_size, N, p, type)
             #ran = rand(Uniform(0, 1), sample_size);
             #ran = QuasiMonteCarlo.sample(sample_size,0,1,LatinHypercubeSample())
             #s = zeros(sample_size,1)
-            ind_charge = rand(Bernoulli(p), 1)
+            #ind_charge = rand(Bernoulli(p), 1)
+            ind_charge = 1
             #idx = randperm(1)
             P = ((idx[j,i]-ran[j,i])/sample_size)*100 # probability of the cdf
-            s[j,i] = (ind_charge * percentile(data[ind_cluster,1],P) / 1000)[1]; # Trick for sampling
+            s_aux[j,i] = (ind_charge * percentile(data[ind_cluster,1],P) / 1000)[1]; # Trick for sampling
             #hist_s = histogram(y, label="Sampled", bins=:sqrt)
             #hist_s = histogram!(s, label="Re-sampled QMC", fillalpha=0.7, bins=:sqrt)
         end
     end
+    QMC_Bern = QuasiMonteCarlo.LatinHypercubeSample(rand(Bernoulli(p),(sample_size,N))).threading
+    s = QMC_Bern.*s_aux
     return s, cluster
 end
+
+y = Vector{Float64}()
+for s in 200:200:5000
+    y = push!(y,mean(QMC_LHS_empirical(Data_smoker, s, 3, 0.2, 1)[1][:,1]))
+end 
+
+plot(collect(200:200:5000), y)
+
+
 
 function calcula_receita(D::Matrix, cluster::Vector, dict::Dict, unique::Bool, type::String)
 
@@ -151,95 +165,139 @@ Data = CSV.read("Databases/contracts.csv", DataFrame)
 
 unique(Data[!,["Category", "Cluster"]])
 
-# Hyperparameters
-N = 2 #No de accidentes
-α = 0.95 #quantil do Cvar
-λ = 0.25 #peso do cvar na função objetivo
-τ = 1.0 #loading factor
-Ti = 0.1
-Tf = 0.5
+
+HyperP = Dict()
+#Base scenario
+HyperP["N"] = 3
+HyperP["α"] = 0.95
+HyperP["λ"] = 0.25
+HyperP["τ"] = 2.5
+HyperP["Ti"] = 1.0
+HyperP["Tf"] = 2.0
+HyperP["M"] = [1e8, 1e8, 1e8]
+HyperP["p"] = 0.1
+HyperP["sample_size"] = 700
+
 M = [1e8, 1e8, 1e8]
-p = 0.05
 
-#Sorting out data for each cluster
+function optimal_contract_point(N, α, λ, τ, Ti, Tf, p, sample_size)
 
-# Getting index
-ind_smoker = findall(i -> i > 1, Data.Cluster)
-ind_nsmoker = findall(i -> i == 1, Data.Cluster)
-#ind0 = findall(i -> i == 0, Data[:,8])
-ind1 = findall(i -> i == 1, Data.Cluster)
-ind2 = findall(i -> i == 2, Data.Cluster)
-ind3 = findall(i -> i == 3, Data.Cluster)
+    #Sorting out data for each cluster
 
-Data_smoker = Data[ind_smoker,:]
-Data_nsmoker = Data[ind_nsmoker,:]
-#Data0 = Data[ind0,:]
-Data1 = Data[ind1, :]
-Data2 = Data[ind2, :]
-Data3 = Data[ind3, :]
+    # Getting index
+    ind_smoker = findall(i -> i > 1, Data.Cluster)
+    ind_nsmoker = findall(i -> i == 1, Data.Cluster)
+    #ind0 = findall(i -> i == 0, Data[:,8])
+    ind1 = findall(i -> i == 1, Data.Cluster)
+    ind2 = findall(i -> i == 2, Data.Cluster)
+    ind3 = findall(i -> i == 3, Data.Cluster)
 
-resultados = zeros(9, 6)
+    Data_smoker = Data[ind_smoker,:]
+    Data_nsmoker = Data[ind_nsmoker,:]
+    #Data0 = Data[ind0,:]
+    Data1 = Data[ind1, :]
+    Data2 = Data[ind2, :]
+    Data3 = Data[ind3, :]
 
-Random.seed!(123)
-sample_size = 500
+    resultados = zeros(9, 6)
 
-D_unique, cluster_unique = QMC_LHS_empirical(Data, sample_size, N, p, 2)
-D_smoker, cluster_smoker = QMC_LHS_empirical(Data_smoker, sample_size, N, p, 1)
-D_nsmoker, cluster_nsmoker = QMC_LHS_empirical(Data_nsmoker, sample_size, N, p, 1)
+    Random.seed!(123)
 
-D1, cluster1 = QMC_LHS_empirical(Data1, sample_size,N, p,2) #Non-smoker
-D2, cluster2 = QMC_LHS_empirical(Data2, sample_size,N, p,2) #Soker+Obese
-D3, cluster3 = QMC_LHS_empirical(Data3, sample_size,N, p,2) #Smoker+Non Obese
 
-D = [D_unique, D_smoker, D_nsmoker, D1,D2,D3]
+    D_unique, cluster_unique = QMC_LHS_empirical(Data, sample_size, N, p, 2)
+    D_smoker, cluster_smoker = QMC_LHS_empirical(Data_smoker, sample_size, N, p, 1)
+    D_nsmoker, cluster_nsmoker = QMC_LHS_empirical(Data_nsmoker, sample_size, N, p, 1)
 
-for j in 1:6
-    time = @elapsed π0, T0 , z0, exp, cvar = estima_contrato(D[j])
-    resultados[:,j] = vcat(time,p, π0, T0, z0, exp, cvar)
+    D1, cluster1 = QMC_LHS_empirical(Data1, sample_size,N, p,2) #Non-smoker
+    D2, cluster2 = QMC_LHS_empirical(Data2, sample_size,N, p,2) #Soker+Obese
+    D3, cluster3 = QMC_LHS_empirical(Data3, sample_size,N, p,2) #Smoker+Non Obese
+
+    D = [D_unique, D_smoker, D_nsmoker, D1,D2,D3]
+
+    for j in 1:6
+        time = @elapsed π0, T0 , z0, exp, cvar = estima_contrato(D[j], α, λ, τ, Ti, Tf)
+        resultados[:,j] = vcat(time,p, π0, T0, z0, exp, cvar)
+    end
+
+    resultados
+
+    dict_contracts = Dict()
+    dict_contracts["1"] = Dict()
+    dict_contracts["2"] = Dict()
+    dict_contracts["unique"] = resultados[3:6,1]
+    dict_contracts["1"][1] =  resultados[3:6,2] # Smoker
+    dict_contracts["1"][0] =  resultados[3:6,3] # Non-smoker
+    dict_contracts["2"][1] =  resultados[3:6,4] # Non-smoker
+    dict_contracts["2"][2] =  resultados[3:6,5] # Smoker + Non Obese
+    dict_contracts["2"][3] =  resultados[3:6,6] # Smoker + Obese
+
+    r = resultados'
+
+
+    return DataFrame(Matrix{}(r), [:Time, :Probability, :RiskPremium, :T0, :T1, :T2, :Z, :Expected, :CVaR])
+
 end
 
 
+"""EXPERIMENTS"""
 
-dict_contracts = Dict()
-dict_contracts["1"] = Dict()
-dict_contracts["2"] = Dict()
-dict_contracts["unique"] = resultados[3:6,1]
-dict_contracts["1"][1] =  resultados[3:6,2] # Smoker
-dict_contracts["1"][0] =  resultados[3:6,3] # Non-smoker
-dict_contracts["2"][0] =  resultados[3:6,4] # Non-smoker
-dict_contracts["2"][1] =  resultados[3:6,5] # Smoker + Non Obese
-dict_contracts["2"][2] =  resultados[3:6,6] # Smoker + Obese
 
-r = resultados'
+#optimal_contract_point(3, 0.95, 0.25, 2.1, 0.1, 2.0, 0.1, 1200)
+
+for p in 0.1:0.05:2.0
+    print(p)
+    CSV.write("ExperimentBase_Ti_$Ti.csv", optimal_contract_point(3, 0.95, 0.25, 2.5, Ti, 2.0, p, 700))
+end
+
+for Ti in 0.1:0.05:2.0
+    print(Ti)
+    CSV.write("ExperimentBase_Ti_$Ti.csv", optimal_contract_point(3, 0.95, 0.25, 2.5, Ti, 2.0, 0.1, 700))
+end
+
+τ
+# Loading factor
+
+@time Threads.@threads for τ in 1.6:0.05:3.5
+    print("$τ/n")
+    CSV.write("ExperimentBase_lf_$τ.csv", optimal_contract_point(3, 0.90, 0.15, τ, 0.05, 2.0, 0.1, 1300))
+end
+
+res = optimal_contract_point(3, 0.90, 0.20, 2.5, 0.05, 2.0, 0.1, 1300)
 
 #D_teste[1,1]
+test_size = 5000
 
 Random.seed!(321)
-D_teste, cluster_teste = QMC_LHS_empirical(Data, 300,N, p, 2)
-cluster_teste1 = zeros(300)
+D_teste, cluster_teste = QMC_LHS_empirical(Data, test_size,N, p, 2)
+cluster_teste1 = zeros(test_size)
 
-for i in 1:300
-    if cluster_teste[i] == 1 || cluster_teste[i] == 3
+for i in 1:test_size
+    if cluster_teste[i] == 2 || cluster_teste[i] == 3
         cluster_teste1[i] = 1
     else
         cluster_teste1[i] = 0
     end
 end
 
-receita_unique, indenizacao_unique = calcula_receita(D_teste, cluster_teste, dict_contracts, true, "2")
+receita_unique, indenizacao_unique = calcula_receita(D_teste, cluster_teste, dict_contracts, true, "unique")
+
+
 receita_smoker, indenizacao_smoker = calcula_receita(D_teste, cluster_teste1, dict_contracts, false, "1")
 receita_cluster, indenizacao_cluster = calcula_receita(D_teste, cluster_teste, dict_contracts, false, "2")
 
 density(receita_unique, label = "Contrato único", linewidth = 1.5)
-density!(receita_smoker, label = "Contrato por hábito de fumar", legend= :topleft , linewidth = 1.5)
-density!(receita_cluster, label = "Contrato por cluster", legend= :topleft , linewidth = 1.5)
+density!(receita_smoker, label = "Contrato por hábito de fumar", legend= :topright , linewidth = 1.5)
+density!(receita_cluster, label = "Contrato por cluster", legend= :topright , linewidth = 1.5)
 title!("Comparativo das receitas")
 
+# Graph
 receita = vcat(receita_unique, receita_smoker, receita_cluster)
-contrato = vcat(repeat(["Contrato único"], inner = 300), repeat(["Dois contratos"], inner = 300), repeat(["Quatro contratos"], inner = 300))
+contrato = vcat(repeat(["Contrato único"], inner = test_size), repeat(["Dois contratos"], inner = test_size), repeat(["Tres contratos"], inner = test_size))
 
-StatsPlots.violin(contrato, receita, color = "gray", alpha = 0.5)
-boxplot!(contrato, receita, colour = "blue", legend = false)
+
+StatsPlots.violin(contrato, log.(receita.+100), color = "gray", alpha = 1)
+boxplot!(contrato, log.(receita.+100), colour = "blue", legend = false)
+hline!(hline!([log(100)]))
 title!("Distribuição das receitas (p = $p)")
 yaxis!("Receita")
 
@@ -257,70 +315,3 @@ ApproximateSignedRankTest(receita_smoker, receita_cluster)
 ExactSignedRankTest(receita_unique, receita_smoker)
 ExactSignedRankTest(receita_unique, receita_cluster)
 ExactSignedRankTest(receita_smoker, receita_cluster)
-
-
-
-
-
-
-
-boxplot(receita_unique, label = "Contrato único", linewidth = 1.5)
-boxplot!(receita_smoker, label = "Contrato por hábito de fumar", legend= :topleft , linewidth = 1.5)
-boxplot!(receita_cluster, label = "Contrato por cluster", legend= :topleft , linewidth = 1.5)
-StatsPlots.violin!(receita_unique, label = "Contrato único", linewidth = 1.5)
-StatsPlots.violin!(receita_smoker, label = "Contrato por hábito de fumar", legend= :topleft , linewidth = 1.5)
-StatsPlots.violin!(receita_cluster, label = "Contrato por cluster", legend= :topleft , linewidth = 1.5)
-title!("Comparativo das receitas")
-
-
-StatsPlots.violin(receita_unique, label = "Contrato único", linewidth = 1.5)
-boxplot!(receita_unique, label = "Contrato único", linewidth = 1.5)
-
-summarystats(receita_unique)
-summarystats(receita_smoker)
-summarystats(receita_cluster)
-
-
-Data[ind_smoker, :]
-
-
-
-
-
-
-Plots.plot(S1, resultados[6,1,:])
-Plots.hline!([12.508336242883423])
-Plots.savefig("ConvergenceRep1.png")
-CSV.write("ConvergenceRep1.csv",  Tables.table(resultados[6,1,:]), writeheader=true)
-
-S2 = collect(5:5:300)
-Rep2 = 100
-
-resultados = zeros(6, Rep2, length(S2))
-
-# Hyperparameters
-N = 10
-α = 0.95 #quantil do Cvar
-λ = 0.5 #peso do cvar na função objetivo
-τ = 0.3 #loading factor
-Ti = 10
-Tf = 150
-M = [1e8, 1e8, 1e8]
-p = 0.3
-
-for j in 1:length(S2)
-    for r in 1:Rep2
-        D = QMC_LHS_empirical(charges, S2[j])[1]
-        @time π0, T0 , z0 = estima_contrato(D)
-        resultados[:,r,j] = vcat(p, π0, T0, z0)
-    end
-end
-
-
-Plots.plot(S2, resultados[6,1,:])
-Plots.hline!([12.508336242883423])
-Plots.savefig("ConvergenceRep100.png")
-
-for j in 1:length(S2)
-    CSV.write("ConvergenceRep100_$j.csv",  Tables.table(resultados[6,:,j]), writeheader=true)
-end
